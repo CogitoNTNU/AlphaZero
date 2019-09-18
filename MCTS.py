b1370277a5bf02632c7a7540de364983cee0d1e9
Dilawar Mahmood <dilawarm@stud.ntnu.no>
2019-09-18 21:14:26 +0200

    MCTS

--- MCTS.py
+++ MCTS.py
@@ -4,19 +4,41 @@ import random
 import numpy as np
 # from Othello import Gamelogic
 from TicTacToe import Gamelogic
-import loss
+#import loss
 import collections
 
+C_PUCT = sqrt(2)
 
 # OBS: when the game is over it the algorithm expects that it is none to move
-
+class Node:
+    def __init__(self, parent, t=0, n=0, state):
+        self.parent = parent
+        if parent:
+            parent.add_child(self)
+        self.t = t
+        self.n = n
+        self.state = state
+        self.children = []
+    
+    def get_parent(self):
+        return self.parent
+    
+    def add_child(self, child):
+        self.children.append(child)
+    
+    def is_leaf_node(self):
+        if len(children) == 0:
+            return True
+        return False
 
 class MCTS:
-    def __init__(self):
-        pass
+    
+    def __init__(self, tree, start_state):
+        self.tree = Node(None, start_state)
+        self.start_state = start_state
 
     def reset_search(self):
-        pass
+        self.tree = Node(None, start_state)
 
     # Setting the game the MCTS will be used on
     def set_game(self, game):
@@ -27,8 +49,11 @@ class MCTS:
         pass
 
     # Returning a dictionary with action as key and visit number as value
-    def get_action_numbers(self):
-        pass
+    def get_action_numbers(self, state):
+        action_numbers = {}
+        node = None
+        while 
+
 
     # Returning the prior probabilities of a state, also known as the "raw" NN predictions
     def get_prior_probabilities(self, state):
@@ -47,8 +72,15 @@ class MCTS:
     def get_temperature_move(self, state):
         pass
 
-    def get_most_seached_move(self, state):
-        pass
+    def get_most_searched_move(self, state):
+        actions = get_action_numbers(state)
+        most_searched_move = 0
+        max = -1
+        for action in actions:
+            if actions[action] > max:
+                most_searched_move = action
+                max = actions[action]
+        return most_searched_move
 
     # Executing MCTS search a "number" times
     def search_series(self, number):
@@ -57,3 +89,10 @@ class MCTS:
     # Executing a single MCTS search: Selection-Evaluation-Expansion-Backward pass
     def search(self):
         pass
+
+    def PUCT(self, state, action):
+        actions = get_action_numbers()
+        N = actions[action]
+
+        U = C_PUCT * get_prior_probabilities(state)*
+        return 

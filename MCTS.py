d0e084a33659d9eb47e47cbc57c7d0c87d1aa00b
joulebit <45593399+joulebit@users.noreply.github.com>
2019-09-19 15:52:43 +0200

    MCTS

--- MCTS.py
+++ MCTS.py
@@ -11,13 +11,14 @@ C_PUCT = sqrt(2)
 
 # OBS: when the game is over it the algorithm expects that it is none to move
 class Node:
-    def __init__(self, parent, t=0, n=0, state):
+    def __init__(self, parent, t=0, n=0, action):
         self.parent = parent
         if parent:
             parent.add_child(self)
         self.t = t
         self.n = n
-        self.state = state
+        self.last_action = action
+        self.board_state = parent.get_board_state.execute_move(action)
         self.children = []
     
     def get_parent(self):
@@ -30,6 +31,18 @@ class Node:
         if len(children) == 0:
             return True
         return False
+    
+    def get_board_state(self):
+        return self.state
+    
+    def get_last_action(self):
+        return self.last_action
+    
+    def get_times_visited(self):
+        return self.n
+    
+    def get_total_values(self):
+        return self.t
 
 class MCTS:
     
@@ -51,8 +64,10 @@ class MCTS:
     # Returning a dictionary with action as key and visit number as value
     def get_action_numbers(self, state):
         action_numbers = {}
-        node = None
-        while 
+        node = state
+        for child in node.children:
+            action_numbers[child.last_action] = child.get_times_visited
+
 
 
     # Returning the prior probabilities of a state, also known as the "raw" NN predictions
@@ -92,7 +107,17 @@ class MCTS:
 
     def PUCT(self, state, action):
         actions = get_action_numbers()
-        N = actions[action]
 
-        U = C_PUCT * get_prior_probabilities(state)*
-        return 
+        action_state = Null
+        for child in state.children:
+            if child.get_last_action() == action:
+                action_state = child
+
+
+        N = actions[action]
+        sum_N_potential_actions = sum(actions.values())
+        U = C_PUCT * get_prior_probabilities(state)*math.sqrt(sum_N_potential_actions)/(1+N)
+
+        Q = action_state.get_total_values()/N
+
+        return Q + U

4316576f5c7cade35672b5d2cc415eb96dccfa13
joulebit <45593399+joulebit@users.noreply.github.com>
2019-09-23 09:19:36 +0200

    bugfixes

--- MCTS.py
+++ MCTS.py
@@ -2,6 +2,7 @@ import math
 import time
 import random
 import numpy as np
+import random
 # from Othello import Gamelogic
 from FourInARow import Gamelogic
 #import loss
@@ -34,7 +35,7 @@ class Node:
         return False
     
     def get_board_state(self):
-        return self.state
+        return self.board_state
     
     def get_last_action(self):
         return self.last_action
@@ -52,33 +53,53 @@ class MCTS:
         self.tree.board_state = start_state
         self.start_state = start_state
         self.game = game
+        self.T = 1
 
     def reset_search(self):
         self.tree = Node(None, None)
         self.tree.board_state = self.start_state
+    
+    def find_node_given_state(self, state):
+        correct = None
+        start = self.tree
+
+        def search_nodechildren_for_state(node,state):
+            if node.get_board_state() == state:
+                return node
+            if not node.is_leaf_node():
+                possible_correct = None
+                for child in node.children:
+                    possible_correct = search_nodechildren_for_state(child,state)
+                    if not possible_correct == None:
+                        return possible_correct
+        
+        correct = search_nodechildren_for_state(start,state)
+        return correct
+
 
     # Setting the evaluation algorithm used by the MCTS
     def set_evaluation(self, eval):
         pass
 
     # Returning a dictionary with action as key and visit number as value
-    def get_action_numbers(self, state):
+    def get_action_numbers(self, node_state):
         action_numbers = {}
-        node = state
+        node = node_state
         for child in node.children:
             action_numbers[child.last_action] = child.get_times_visited()
         return action_numbers   
 
     # Returning the prior probabilities of a state, also known as the "raw" NN predictions
-    def get_prior_probabilities(self, state):
-        return raw_NN_predictions(state)
+    def get_prior_probabilities(self, board_state):
+        return raw_NN_predictions(board_state)
 
     # Returning the posterior search probabilities of the search,
     # meaning that the percentages is calculated by: num_exec/total
-    def get_posterior_probabilities(self, state):
+    def get_posterior_probabilities(self, board_state):
+        node = self.find_node_given_state(board_state)
         tot = 0
         post_prob = {}
-        actions = self.get_action_numbers(state)
+        actions = self.get_action_numbers(node)
         for action in actions:
             tot += actions[action]
         for action in actions:
@@ -86,36 +107,36 @@ class MCTS:
         return post_prob
 
     # Returning the temperature probabilities calculated from the number of searches for each action
-    def get_temperature_probabilities(self, state, T):
+    def get_temperature_probabilities(self, node_state):
         pi = {}
-        actions = self.get_action_numbers(state)
+        actions = self.get_action_numbers(node_state)
         for action in actions:
             pi[action] = (actions[action])**(1/T)
         return pi    
 
 
     # Returning a random move proportional to the temperature probabilities
-    def get_temperature_move(self, state):
-        pi = get_temperature_probabilities(state)
+    def get_temperature_move(self, node_state):
+        pi = self.get_temperature_probabilities(node_state)
         pi_sum = 0
         for value in pi:
             pi_sum = pi_sum + value
-        choice = np.rand(0, pi_sum)
+        choice = np.random.uniform(0, pi_sum)
         tellesum = 0
         for i in range(0, len(pi)):
             tellesum = tellesum + pi[i]
             if choice < tellesum:
                 return i
     
-    def evaluate(self, state, to_play):
+    def evaluate(self, board_state, to_play):
         if to_play != 0:
-            value = 1 - self.evaluate(state, 0)
+            value = 1 - self.evaluate(board_state, 0)
         else:
-            value = get_info_from_NN()
+            value = get_info_from_NN(board_state)
             return value
 
-    def get_most_searched_move(self, state):
-        actions = self.get_action_numbers(state)
+    def get_most_searched_move(self, node_state):
+        actions = self.get_action_numbers(node_state)
         most_searched_move = 0
         max = -1    
         for action in actions:
@@ -136,12 +157,12 @@ class MCTS:
         while not node.is_leaf_node():
             best_puct = 0
             for n in node.children:
-                curr_puct = PUCT(n.state, n.action)
+                curr_puct = self.PUCT(n, n.action)
                 if (curr_puct > best_puct):
                     best_child = n
                     best_puct = curr_puct
             node = best_child
-        node.t = get_info_from_NN(node.state)
+        node.t = get_info_from_NN(node.get_board_state())
         valid_moves = self.game.get_moves(node.get_board_state())
         for move in valid_moves:
             child = Node(node, move)
@@ -150,20 +171,20 @@ class MCTS:
     def back_propagate(self, node):
         if node.parent != None:
             node.get_parent.t += node.t
-            back_propagate(node.get_parent)
+            self.back_propagate(node.get_parent)
 
-    def PUCT(self, state, action):
-        actions = self.get_action_numbers()
+    def PUCT(self, node_state, action):
+        actions = self.get_action_numbers(node_state)
 
         action_state = None
-        for child in state.children:
+        for child in node_state.children:
             if child.get_last_action() == action:
                 action_state = child
 
 
         N = actions[action]
         sum_N_potential_actions = sum(actions.values())
-        U = C_PUCT * self.get_prior_probabilities(state)*math.sqrt(sum_N_potential_actions)/(1+N)
+        U = C_PUCT * self.get_prior_probabilities(node_state)*math.sqrt(sum_N_potential_actions)/(1+N)
 
         Q = action_state.get_total_values()/N
 

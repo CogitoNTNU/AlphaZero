402862528efbd944bc05f74dca238f62e30bd8fb
Dilawar Mahmood <40496588+dilawarm@users.noreply.github.com>
2019-10-23 10:53:25 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,10 +5,10 @@ import Files
 
 # from Othello import Gamerendering
 # from Othello import Gamelogic
-#from TicTacToe import Gamelogic
-#from TicTacToe import Config
-from FourInARow import Gamelogic
-from FourInARow import Config
+from TicTacToe import Gamelogic
+from TicTacToe import Config
+#from FourInARow import Gamelogic
+#from FourInARow import Config
 from keras.optimizers import SGD
 from loss import softmax_cross_entropy_with_logits, softmax
 #import NN2
@@ -20,15 +20,15 @@ import numpy as np
 
 # Creating and returning a tree with properties specified from the input
 def get_tree(config, agent, game, dirichlet_noise=True):
-    tree = MCTS.MCTS(game, game.get_board(), agent, config)
-    # tree.dirichlet_noise = dirichlet_noise
-    # tree.NN_input_dim = config.board_dims
-    # tree.policy_output_dim = config.policy_output_dim
-    # tree.NN_output_to_moves_func = config.NN_output_to_moves
-    # tree.move_to_number_func = config.move_to_number
-    # tree.number_to_move_func = config.number_to_move
-    # tree.set_evaluation(agent)
-    # tree.set_game(game)
+    tree = MCTS.MCTS()
+    tree.dirichlet_noise = dirichlet_noise
+    tree.NN_input_dim = config.board_dims
+    tree.policy_output_dim = config.policy_output_dim
+    tree.NN_output_to_moves_func = config.NN_output_to_moves
+    tree.move_to_number_func = config.move_to_number
+    tree.number_to_move_func = config.number_to_move
+    tree.set_evaluation(agent)
+    tree.set_game(game)
     return tree
 
 
@@ -56,15 +56,15 @@ def generate_data(game, agent, config, num_sim=100, games=1):
 
         while not game.is_final():
             tree.reset_search()
-            tree.root.board_state = game.get_board()
+            #tree.root.board_state = game.get_board()
             tree.search_series(num_sim)
             # tree.search_series(10)
             state = game.get_state()
-            temp_move = tree.get_temperature_move(tree.root)
-            print(tree.get_temperature_probabilities(tree.root))
+            temp_move = tree.get_temperature_move(state)
+            print(tree.get_temperature_probabilities(state))
             history.append(temp_move)
-            policy_targets.append(np.array(tree.get_posterior_probabilities()))
-            print(tree.get_prior_probabilities(game.get_board().reshape(1, 6, 7, 2))) #reshape(1,3,3,2)
+            policy_targets.append(np.array(tree.get_posterior_probabilities(state)))
+            print(tree.get_prior_probabilities(state)) #reshape(1,3,3,2)
             print(policy_targets[-1])
             player_moved_list.append(game.get_turn())
             positions.append(np.array(game.get_board()))
@@ -84,8 +84,8 @@ def generate_data(game, agent, config, num_sim=100, games=1):
 
 
 # Training AlphaZero by generating data from self-play and fitting the network
-def train(game, config, num_filters, num_res_blocks, num_sim=125, epochs=50, games_each_epoch=10,
-          batch_size=32, num_train_epochs=10):
+def train(game, config, num_filters, num_res_blocks, num_sim=125, epochs=2000, games_each_epoch=1,
+          batch_size=32, num_train_epochs=3):
     h, w, d = config.board_dims[1:]
     # agent, agent1 = NN2.ResNet.build(h, w, d, num_filters, config.policy_output_dim, num_res_blocks=num_res_blocks)
     agent = ResNet.ResNet.build(h, w, d, num_filters, config.policy_output_dim, num_res_blocks=num_res_blocks)
@@ -108,14 +108,11 @@ def train(game, config, num_filters, num_res_blocks, num_sim=125, epochs=50, gam
         print("Epoch")
         print(x.shape)
         raw = agent.predict(x)
-        for num in range(len(x)):
-            print("targets-predictions")
-            print(y_pol[num], y_val[num])
-            print(softmax(y_pol[num], raw[0][num]), raw[1][num])
 
         agent.fit(x=x, y=[y_pol, y_val], batch_size=min(batch_size, len(x)), epochs=num_train_epochs, callbacks=[])
         print("end epoch")
-        agent.save_weights("Models/"+Config.name+"/"+str(epoch)+".h5")
+        if (epoch % 10 == 0):
+            agent.save_weights("Models/"+Config.name+"/"+str(epoch)+".h5")
     return agent
 
 
@@ -130,5 +127,5 @@ def choose_best_legal_move(legal_moves, y_pred):
         y_pred[best_move] = 0
         return choose_best_legal_move(legal_moves, y_pred)
 
-
-#train(Gamelogic.FourInARow(), Config, 128, 5)
+if __name__ == '__main__':
+    train(Gamelogic.TicTacToe(), Config, 128, 5)

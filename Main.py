147d589c19dc9f807fc863322d49fa9afee2f83c
Agnar22 <agnar.bjornstad@gmail.com>
2019-10-27 17:41:31 +0100

    Added multiprocessing and copied GameGenerator from the multiprocessing branch

--- Main.py
+++ Main.py
@@ -2,21 +2,20 @@
 import ResNet
 import MCTS
 import Files
+import Multiprocessing
 
 from FourInARow import Gamelogic
 from FourInARow import Config
 # from TicTacToe import Gamelogic
 # from TicTacToe import Config
-#from FourInARow import Gamelogic
-#from FourInARow import Config
+# from FourInARow import Gamelogic
+# from FourInARow import Config
 from keras.optimizers import SGD
 from loss import softmax_cross_entropy_with_logits, softmax
-#import NN2
 
 # Importing other libraries
 import numpy as np
 
-# TODO: game outcome not propagated correctly?
 
 # Creating and returning a tree with properties specified from the input
 def get_tree(config, agent, game, dirichlet_noise=True):
@@ -32,82 +31,142 @@ def get_tree(config, agent, game, dirichlet_noise=True):
     return tree
 
 
+def get_game_object():
+    return Gamelogic.TicTacToe()
+
+
+class GameGenerator:
+    def __init__(self, config):
+        self.game = get_game_object()
+        self.tree = get_tree(config, None, self.game)
+        self.history = []
+        self.policy_targets = []
+        self.player_moved_list = []
+        self.positions = []
+
+    def run_part1(self):
+        return self.tree.search_part1()
+
+    def run_part2(self, result):
+        self.tree.search_part2(result)
+        return self
+
+    def execute_best_move(self):
+        temp_move = self.tree.get_temperature_move(self.game.get_state())
+
+        self.history.append(temp_move)
+        self.policy_targets.append(np.array(self.tree.get_posterior_probabilities(self.game.get_state())))
+        self.player_moved_list.append(self.game.get_turn())
+        self.positions.append(np.array(self.game.get_board()))
+        self.game.execute_move(temp_move)
+        return self, self.game.is_final()
+
+    def reset_tree(self):
+        self.tree.reset_search()
+
+    def get_results(self):
+        game_outcome = self.game.get_outcome()
+        value_targets = [game_outcome[x] for x in self.player_moved_list]
+        return np.array(self.positions), np.array(self.policy_targets), np.array(value_targets)
+
+
 # Generating data by self-play
-def generate_data(game, agent, config, num_sim=100, games=1):
-    tree = get_tree(config, agent, game)
+def generate_data(result_queue, game, agent, config, num_sim=1, games=1, num_search=130):
+    # tree = get_tree(config, agent, game)
+
+    game_generators = [GameGenerator(config) for _ in range(num_sim)]
 
     x = []
     y_policy = []
     y_value = []
 
-    for curr_game in range(games):
+    while len(game_generators):
+        for game_generator in game_generators:
+            game_generator.reset_tree()
+        res = []
+        for i in range(num_search):
+            res = [game_generator.run_part1() for game_generator in game_generators]
+            batch = np.array(res)
+            results = agent.predict(batch)
+            # print(np.array([results[0][0], results[1][0]]))
+            # print(np.array([results[0][i], [results[1][i]]])[0][1])
+            res = [game_generators[i].run_part2(np.array([results[0][i], [results[1][i]]])) for i in range(len(res))]
+            # print("har gjort 100 søk")
+        res = [game_generator.execute_best_move() for game_generator in game_generators]
+        game_generators = []
+        finished_games = []
+        for game_generator, finished in res:
+            if finished:
+                finished_games.append(game_generator)
+                continue
+            game_generators.append(game_generator)
+        game_results = [game_generator.get_results() for game_generator in finished_games]
+        for history, policy_targets, value_targets in game_results:
+            x.append(history)
+            y_policy.append(policy_targets)
+            y_value.append(value_targets)
+    result_queue.put([np.array(x), np.array(y_policy), np.array(y_value)])
+    # return np.array(x), np.array(y_policy), np.array(y_value)
 
-        game.__init__()
-        # game.execute_move(0)
-        # game.execute_move(3)
-        # game.execute_move(1)
-        # game.execute_move(4)
-        # game.execute_move(6)
-        # game.execute_move(7)
-        history = []
-        policy_targets = []
-        player_moved_list = []
-        positions = []
 
-        while not game.is_final():
-            tree.reset_search()
-            #tree.root.board_state = game.get_board()
-            print("num_sim", num_sim)
-            tree.search_series(num_sim)
-            # tree.search_series(10)
-            state = game.get_state()
-            temp_move = tree.get_temperature_move(state)
-            print("move:", temp_move)
-            print("temp_probs:", tree.get_temperature_probabilities(state))
-            history.append(temp_move)
-            policy_targets.append(np.array(tree.get_posterior_probabilities(state)))
-            print("prior_probs:", tree.get_prior_probabilities(state)) #reshape(1,3,3,2)
-            print("pol_targets", policy_targets[-1])
-            player_moved_list.append(game.get_turn())
-            positions.append(np.array(game.get_board()))
-
-            game.execute_move(temp_move)
-
-        game_outcome = game.get_outcome()
-        value_targets = [game_outcome[x] for x in player_moved_list]
-        print("val_targets:", value_targets)
-
-        x = x + positions
-        y_policy = y_policy + policy_targets
-        y_value = y_value + value_targets
-
-        print("History:", history)
-
-    return np.array(x), np.array(y_policy), np.array(y_value)
+# # Generating data by self-play
+# def generate_data(game, agent, config, num_sim=100, games=1):
+#     tree = get_tree(config, agent, game)
+#
+#     x = []
+#     y_policy = []
+#     y_value = []
+#
+#     for curr_game in range(games):
+#
+#         game.__init__()
+#         history = []
+#         policy_targets = []
+#         player_moved_list = []
+#         positions = []
+#
+#         while not game.is_final():
+#             tree.reset_search()
+#             print("num_sim", num_sim)
+#             tree.search_series(num_sim)
+#             state = game.get_state()
+#             temp_move = tree.get_temperature_move(state)
+#             print("move:", temp_move)
+#             print("temp_probs:", tree.get_temperature_probabilities(state))
+#             history.append(temp_move)
+#             policy_targets.append(np.array(tree.get_posterior_probabilities(state)))
+#             print("prior_probs:", tree.get_prior_probabilities(state)) #reshape(1,3,3,2)
+#             print("pol_targets", policy_targets[-1])
+#             player_moved_list.append(game.get_turn())
+#             positions.append(np.array(game.get_board()))
+#
+#             game.execute_move(temp_move)
+#
+#         game_outcome = game.get_outcome()
+#         value_targets = [game_outcome[x] for x in player_moved_list]
+#         print("val_targets:", value_targets)
+#
+#         x = x + positions
+#         y_policy = y_policy + policy_targets
+#         y_value = y_value + value_targets
+#
+#         print("History:", history)
+#
+#     return np.array(x), np.array(y_policy), np.array(y_value)
 
 
 # Training AlphaZero by generating data from self-play and fitting the network
 def train(game, config, num_filters, num_res_blocks, num_sim=400, epochs=200, games_each_epoch=10,
           batch_size=32, num_train_epochs=3):
     h, w, d = config.board_dims[1:]
-    # agent, agent1 = NN2.ResNet.build(h, w, d, num_filters, config.policy_output_dim, num_res_blocks=num_res_blocks)
     agent = ResNet.ResNet.build(h, w, d, num_filters, config.policy_output_dim, num_res_blocks=num_res_blocks)
     agent.compile(loss=[softmax_cross_entropy_with_logits, 'mean_squared_error'],
                   optimizer=SGD(lr=0.001, momentum=0.9))
     agent.summary()
 
-    # game.__init__()
-    # game.execute_move(0)
-    # game.execute_move(3)
-    # game.execute_move(1)
-    # game.execute_move(4)
-    # game.execute_move(6)
-    # game.execute_move(7)
-
-    # print(agent.predict(game.get_board().reshape(1,3,3,2)))
-
     for epoch in range(epochs):
-        x, y_pol, y_val = generate_data(game, agent, config, num_sim=num_sim, games=games_each_epoch)
+        x, y_pol, y_val=Multiprocessing.multiprocess_funcion(8, game, agent, Config)
+        # x, y_pol, y_val = generate_data(game, agent, config, num_sim=num_sim, games=games_each_epoch)
         print("Epoch")
         print(x.shape)
         raw = agent.predict(x)
@@ -116,7 +175,7 @@ def train(game, config, num_filters, num_res_blocks, num_sim=400, epochs=200, ga
         agent.fit(x=x, y=[y_pol, y_val], batch_size=min(batch_size, len(x)), epochs=num_train_epochs, callbacks=[])
         print("end epoch")
         if (epoch % 10 == 0):
-            agent.save_weights("Models/"+Config.name+"/"+str(epoch)+"_batch.h5")
+            agent.save_weights("Models/" + Config.name + "/" + str(epoch) + "_batch.h5")
     return agent
 
 
@@ -130,6 +189,7 @@ def choose_best_legal_move(legal_moves, y_pred):
     else:
         y_pred[best_move] = 0
         return choose_best_legal_move(legal_moves, y_pred)
+
 
 if __name__ == '__main__':
     train(Gamelogic.FourInARow(), Config, 128, 5)
---
+++ Multiprocessing.py
@@ -0,0 +1,16 @@
+import Main
+from multiprocessing import Process, Queue
+import numpy as np
+
+
+def multiprocess_funcion(num_processes, game, agent, config, num_sim=20, games=20, num_search=400):
+    result_queue = Queue()
+
+    workers = [Process(target=Main.generate_data,
+                       args=(result_queue, game, agent, config))
+               for _ in range(num_processes)]
+    for worker in workers: worker.start()
+    for worker in workers: worker.join()
+    result = [result_queue.get() for _ in num_processes]
+    return np.concatenate([x[0] for x in result]), np.concatenate([x[1] for x in result]),\
+           np.concatenate([x[2] for x in result])
